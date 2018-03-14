9b1f41d60b2b44cedf243cf7fb1d3237e13fd4bd
shiv <shiv.sheladiya16@gmail.com>
2018-03-14 23:32:25 +0530

    delete record operation

--- node/program/insert-record/delete.js
+++ node/program/insert-record/delete.js
@@ -1,4 +1,4 @@
-// node.js update single record from collection (mongodb)
+// node.js delete single record from collection (mongodb)
 
 var http = require('http');
 var MongoClient = require('mongodb').MongoClient;
@@ -8,11 +8,10 @@ http.createServer(function(req, res){
     MongoClient.connect(url, function(err, dcont){
         if(err) throw err;
         var db = dcont.db('college');
-        var odata = { "dname": "mehul" };
-        var ndata = { $set: { "dname": "raju", "demail": "raju.patel@gmail.com", "daddress": "rajkot" } };
-        db.collection('student').updateOne(odata, ndata, function (err, result){
+        var ddata = { "demail": "raju.patel@gmail.com" };
+        db.collection('student').deleteOne(ddata, function(err, result){
             if(err) throw err;
-            console.log("record updated");
+            console.log('1 record deleted');
         });
     });
 }).listen(8080);
