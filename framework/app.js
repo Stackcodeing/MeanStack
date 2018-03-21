b4a2abee84ef529d15c76008a4bad26a0539987f
shiv <shiv.sheladiya16@gmail.com>
2018-03-21 23:29:55 +0530

    express freamework commit

---
+++ framework/app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+app.get('/', function(req, res){
+    res.send('<h1>First Express program </h1>');
+});
+
+var Server = app.listen(8080, function(){
+    console.log('listen to port 8080');
+});
+
+
+
+// var http = require('http');
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.write('hello world');
+//     res.write('Receive request from server' + req.url);
+//     res.end();
+// }).listen(8080);
